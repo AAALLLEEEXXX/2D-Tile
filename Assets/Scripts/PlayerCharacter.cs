d8d58d3c8a628af2555913babd724f7e75c01024
a.gavrikov <a.gavrikov@gamingsense.ru>
2021-02-21 18:05:27 +0300

    Персонаж и анимации

---
+++ Assets/Scripts/PlayerCharacter.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacter : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed;
+
+    private Rigidbody2D _rigidbody;
+    private Animator _animator;
+    private SpriteRenderer _spriteRenderer;
+
+    private Vector2 _moveVelocity;
+
+    private static readonly int Speed = Animator.StringToHash("Speed");
+    private static readonly int Strike = Animator.StringToHash("Strike");
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        var moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        _moveVelocity = moveInput.normalized * _speed;
+
+        _animator.SetFloat(Speed, Mathf.Abs(_moveVelocity.x));
+
+        _spriteRenderer.flipX = _moveVelocity.x < 0;
+
+        if (Input.GetKeyDown(KeyCode.F))
+            _animator.SetTrigger(Strike);
+    }
+}
